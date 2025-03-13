a11b37156e5c5ce90a4460bdaf1b299dc47f37f5
Egor Malyshkin <malyshkin@hightech.group>
2025-03-13 19:45:49 +0300

    second step

---
+++ dist/wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿window.showElements = function () {
+    setTimeout(() => document.getElementById("loadingText").classList.add("visible"), 500);
+    setTimeout(() => document.getElementById("errorText").classList.add("visible"), 2000);
+    setTimeout(() => document.getElementById("glitchImageContainer").classList.add("visible"), 4000);
+    setTimeout(() => document.getElementById("finalText").classList.add("visible"), 6000);
+};
